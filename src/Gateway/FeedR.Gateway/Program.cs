ff0852c33bdc4e28a9747f8e7346c09dced63a5f
Ashkan <Ashkan@DESKTOP-Q7SLNT3>
2022-10-12 00:26:44 +0330

    Add ApiGateway feature with yarp for every projects

--- src/Gateway/FeedR.Gateway/Program.cs
+++ src/Gateway/FeedR.Gateway/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("yarp"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "FeedR Gateway");
-
+app.MapReverseProxy();
 app.Run();
